4cdfd6628d4f54f6aeab831f775bf881d8be8369
chrismlusk <chris.m.lusk@gmail.com>
2016-03-31 16:01:24 -0400

    trim script to reduce steps

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# wikitrump
+# WikiTrump
 
 A Twitter bot for Trump nonsense, because The Donald just doesn't attack enough things in this world.
--- script.py
+++ script.py
@@ -1,17 +1,16 @@
+from tokens import tokens
 import requests
 import random
 from bs4 import BeautifulSoup
-from tokens import tokens
+from PIL import Image
+from resizeimage import resizeimage
 from twython import Twython
 import os
-import time
-
 
 BASE_URL = 'https://en.wikipedia.org'
-RANDOM = '/wiki/Special:Random'
+GET_RANDOM = '/wiki/Special:Random'
 
 exceptions = [
-    '(',
     ',',
     'List',
     'list',
@@ -23,237 +22,166 @@ exceptions = [
 ]
 adjectives = [
     'weak',
-    'pathetic',
     'loser',
     'lousy',
     'third-rate',
-    'LOSER',
-    'dumb',
     'clueless',
     'failing',
     'overrated',
 ]
 insults = [
-    'is ruining the country we love',
-    'has destroyed our great nation',
-    'is clueless',
-    'is so average in every way',
-    'is very politically correct',
-    'is very hostile, folks',
-    'had a failed career',
-    'is a cheater',
-    'is in total freefall',
-    'has no common sense',
-    'is such a total failure to us all',
-    'was terrible',
-    'is stealing our jobs',
-    'is TERRIBLE for America',
-    'has failed miserably',
-    'has done nothing. I\'m no fan'
-    'is just a terrible, terrible thing',
-    'can\'t make AMERICA GREAT like I can',
-    'has got a lot of problems',
-    'has problems - many of them',
-    'has a lot of problems',
-    'is such a dope',
-    'is a total lightweight',
-    'is a flunkie and a real dummy',
-    'is not a leader! Such a disaster',
-    'just doesn\'t have a clue',
-    'is really boring',
-    'has been so wrong & is bad',
-    'has no talent',
-    'has no talent (& is boring)',
-    'is very dishonest',
-    'is total garbage',
-    'is such a hypocrite',
-    'is the WORST EVER',
-    'has never won at anything',
-    'won\'t apologize to me',
-    'better hope I don\'t sue! I WILL win',
-    'is dumber than a rock',
-    'did very badly',
-    'just doesn\'t have IT',
-    'has zero cred',
-    'has no credibility',
-    'failed us very badly',
-    'is a sad sack (and boring)',
-    'is too soft to be a leader',
-    'is so biased against me',
-    'should be ashamed',
-    'did such a horrible job',
-    'can\'t be trusted',
-    'is desperate for attention',
-    'is... I shouldn\'t say it... is a pussy',
-    'has no imagination',
-    'is such a joke. A total loser',
-    'is completely wrong',
-    'might be backed by ISIS',
-    'never wins at anything, folks',
-    'is very bad',
-    'is such a major sleaze',
-    'is making us very, very weak',
-    'is RUINING our nation, folks',
-    'is not good',
-    'is killing us',
-    'is killing us! It\'s very bad',
-    'is doing many bad things',
-    'is very unethical, folks',
-    'just doesn\'t know how to win',
-    'should be fired like a DOG',
-    'is a total low life',
-    'will NEVER Make America Great Again',
-    'is very uncomfortable',
-    'is all talk no action',
-    'is very dishonest - a FRAUD',
-    'knows nothing',
-    'will be fired like a dog',
-    'is just hopeless',
-    'is so easy to beat',
-    'is one of the worst in history',
-    'is wasting time & money',
-    'is truly weird',
-    'is a zero',
-    'is a hater',
-    'is a major hater',
-    'is a HATER',
-    'is a hater (and very boring)',
-    'is such a hater',
-    'is a total hater',
-    'is a hater & a racist',
-    'has no fans',
-    'did really poorly on television',
-    'is a major lightweight',
-    'should be forced to take an IQ test',
-    'needs to give up',
-    'has the worst record',
-    'treats America like trash',
-    'is just another failure',
-    'is not as smart as me',
-    'knows nothing about business',
-    'is a WACKO',
-    'is a sicko',
-    'let us down very badly',
-    'is even dumber than we thought',
-    'is very boring',
-    'has a horrible attitude',
-    'is terrible (and very boring)',
-    'is a fool',
-    'did a terrible job, folks',
-    'is irrelevant',
-    'is a real nut job',
-    'is a real basketcase',
-    'has weird issues. Mental issues',
-    'is wacky',
-    'deserves to be FIRED',
-    'is not nice',
-    '-- what a joke! A real phony, folks',
-    'is absolutely disgraceful',
-    'is totally out of control',
-    'is broken, just like our country',
-    'is terrible (and boring)',
-    'is very boring',
-    'is weak (and looks strange)',
-    'is so ridiculous',
-    'has no guts, no glory',
-    'is very bad for U.S.A',
-    'is poorly rated - a total disaster',
-    'doesn\'t get the ratings I do',
-    'is very boring & lame',
-    'is an outrage',
-    'has never made a profit',
-    'is ripping us off! We are so weak',
-    'is ripping us off',
-    'is deeply troubled',
-    'is totally corrupt',
-    'is killing us',
-    'is killing us in every way',
-    'has totally killed us',
-    'is not our friend',
-    '-- what a fool',
-    'is corrupt. So corrupt, folks',
-    'is totally lost',
-    'is a total mess',
-    'is trying hard to hide from me',
-    'is small and unimportant',
-    'is inaccurate',
-    'looks very strange',
-    'is so wrong, so often',
+    'is ruining this country.',
+    'has destroyed our great nation.',
+    'is clueless. Might be dangerous?',
+    'is so average in every way.',
+    'is killing us with political correctness.',
+    'is very hostile toward me.',
+    'had a failed career.',
+    'is a failure and in total freefall.',
+    'has no common sense.',
+    'is such a total failure.',
+    'is stealing our jobs - and Obama doesn\'t care!',
+    'is TERRIBLE for America!',
+    'has failed miserably.',
+    'is a terrible, terrible thing.',
+    'can\'t make AMERICA GREAT like I will.',
+    'has got a lot of problems.',
+    'has problems. Many respected people say so.',
+    'has many, many problems.',
+    'is a total lightweight.',
+    'is a flunkie and a real dummy.',
+    'just doesn\'t have a clue.',
+    'is really boring.',
+    'has been so wrong & is bad for our country.',
+    'has no talent (and is boring)',
+    'is very dishonest, a total liar.',
+    'is total garbage.',
+    'is such a hypocrite.',
+    'is dumber than a rock.',
+    'did very badly. The worst of all time?',
+    'just doesn\'t have it. I alone can solve.',
+    'has zero cred.',
+    'has no credibility.',
+    'failed us very badly.',
+    'is so biased against me.',
+    'did such a horrible job.',
+    'can\'t be trusted.',
+    'is... I shouldn\'t say it, but... is a pussy.',
+    'is such a joke and a total loser.',
+    'is completely wrong & cannot be trusted.',
+    'could be backed by ISIS? Many people say so.',
+    'never wins at anything.',
+    'is such a major sleaze.',
+    'is making us very, very weak.',
+    'is RUINING our nation like I predicted.',
+    'is not good, not at all.',
+    'has killed us with incompetence.',
+    'is killing us! It\'s terrible.',
+    'needs to be stopped, but weak Washington won\'t act.',
+    'is totally killing us.',
+    'is killing us, folks.',
+    'is doing many bad things.',
+    'is very unethical, folks.',
+    'doesn\'t know how to win.',
+    'should be FIRED like a dog.',
+    'is a total low life.',
+    'will NEVER Make America Great Again.',
+    'is very uncomfortable.',
+    'is all talk no action.',
+    'is very dishonest - a total fraud.',
+    'is just hopeless.',
+    'is one of the worst of all time.',
+    'is a major hater.',
+    'is, frankly, a total hater.',
+    'is a HATER & a LOSER.',
+    'is a hater (and very boring).',
+    'is such a hater.',
+    'is a total hater.',
+    'is a hater & a racist.',
+    'is a major lightweight.',
+    'should be forced to take an IQ test.',
+    'knows nothing about business.',
+    'is a total wacko.',
+    'let us all down very badly.',
+    'is even dumber than we thought.',
+    'is very boring.',
+    'has a horrible attitude.',
+    'is terrible (and very boring).',
+    'is a fool and not trusted by anyone.',
+    'did a terrible job, folks.',
+    'is totally irrelevant.',
+    'is a real nut job.',
+    'is a complete basketcase.',
+    'does nothing but attack me.',
+    'has issues. Many respected people think so.',
+    'has a lot of issues, folks.',
+    'deserves to be FIRED immediately.',
+    'is absolutely disgraceful.',
+    'is totally out of control.',
+    'is broken, just like our country.',
+    'is terrible (and boring).',
+    'is so ridiculous.',
+    'has no guts, no glory.',
+    'gets terrible ratings.',
+    'is poorly rated - a total disaster.',
+    'is very boring & lame.',
+    'is ripping us off. It\'s an outrage.',
+    'is deeply troubled.',
+    'is totally corrupt.',
+    'is a horrow show like I predicted - but nobody listened.',
+    'is KILLING us.',
+    'has totally killed us.',
+    'is not our friend.',
+    'is corrupt. So corrupt, folks.',
+    'is a total mess but refuses to fix anything.',
+    'is small and unimportant.',
+    'is so wrong, so often.',
+    'is horrible and a complete mess.',
     'might be controlled by China?',
-    'is giving away our money to CHINA, folks',
-    'is pure scum',
-    'is so dishonest',
-    'is killing us',
-    'is very untalented',
-    'is considered by many to be the worst',
-    'is incompetent',
-    'is poorly run and managed',
-    'is worthless',
-    'is highly unethical (and boring)',
-    'has become very desperate',
-    'is in total turmoil',
-    'treats me very badly',
-    'is losing',
-    'has sold us out',
-    'is losing to MEXICO and CHINA',
-    'is killing us very badly',
-    'has lost its way',
-    'is so off',
-    'has gone off the rails',
-    'is a waste of time',
-    'only says negative things about me',
-    'is such a troublemaker',
-    'is a money-losing failure',
-    'has no $ and no talent',
-    'is not very good or professional',
-    'is so biased it is disgusting',
-    'has no power',
-    'is just plain dumb',
-    'can\'t get anything right',
-    'is crazy',
-    'is so crazy',
-    '-- CRAZY! Very crazy, folks',
-    'is a waste',
-    'is leading America to slaughter',
-    'is totally ineffective & has been for years',
-    'is such a scam on our country',
-    'is very stupid & highly incompetent',
-    'is running the U.S.A. into the ground',
-    'is nonsense',
-    'is phony',
-    'is killing us - killing our great nation',
-    'is beating us',
-    'is beating us BADLY',
-    'is beating us like dogs',
-    'is a tool of ISIS!!! Many people say that',
-    'is very soft! Zero strength',
-    'is a disgrace',
-    'has a major inferiority complex',
-    'is a dopey clown',
-    'is weak and a Washington puppet',
-    'has no chance',
-    'is wrong on so many subjects',
-    'should be thrown out',
-    'is BORING',
-    'is broken down',
-    'is so wrong',
-    'is bad (and boring)'
+    'is giving away our money to CHINA, folks.',
+    'is so dishonest & biased.',
+    'is just killing us! We don\'t win anymore.',
+    'is a puppet of the politicians.',
+    'is considered by many to be the worst.',
+    'is very incompetent. Many respected people agree.',
+    'is totally worthless and an awful leader.',
+    'is highly unethical and corrupt.',
+    'isn\'t very bright.',
+    'is in total turmoil. I predicted this.',
+    'is killing us very badly.',
+    'is so off, very crazy! Can\'t be trusted.',
+    'is a troublemaker. Could be backed by ISIS?',
+    'is a money-losing failure.',
+    'has no $$ and has no talent.',
+    'is not very good or professional.',
+    'is so biased it is disgusting.',
+    'is very dumb. Could have mental problems?',
+    'can\'t get anything right.',
+    'is so crazy & has no clue.',
+    'is totally ineffective & has been for years.',
+    'is very stupid & highly incompetent.',
+    'is running the U.S.A. into the ground.',
+    'is such a major phony.',
+    'is killing us - killing our great nation.',
+    'is majorly beating us.',
+    'is a tool of ISIS! Many people say that.',
+    'is very soft! No strength at all.',
+    'is a major disgrace.',
+    'has a major inferiority complex.',
+    'is a dopey clown.',
+    'is wrong on so many subjects.',
 ]
 endings = [
     'SAD',
-    'STUPID',
     'WOW',
     'Unbelieveable',
-    'Seriously',
     'So sad',
     'Very sad',
-    'Can you believe it?',
-    'Dummy',
-    'What a dope',
     'Shameful',
     'Awful',
     'What a joke',
-    'It\'s true!!'
+    'Disgusting',
+    'How pathetic',
 ]
 
 
@@ -266,115 +194,90 @@ def get_tokens():
 
 
 def get_page(url_ending):
-    url = BASE_URL + url_ending
-    response = requests.get(url).content
-    soup = BeautifulSoup(response, 'lxml')
-    return soup
+    return BeautifulSoup(requests.get(BASE_URL + url_ending).content, 'lxml')
 
 
 def get_title(scraped_page):
     title = scraped_page.find(id='firstHeading')
     if title:
-        return title.get_text()
-    else:
-        False
+        title = title.get_text()
+        if any(char in title for char in exceptions):
+            return False
+        else:
+            return title
 
 
 def get_image(scraped_page):
-    table = scraped_page.find('table', class_='infobox')
-    if table:
-        image_thumb = table.find('img')
-        if image_thumb and image_thumb.has_attr('data-file-width'):
-            if image_size_correct(image_thumb):
-                image_thumb_tag = scraped_page.find('a', class_='image')
-                if image_thumb_tag:
-                    image_url = get_page(image_thumb_tag['href']).find(class_='fullImageLink').find('a')['href']
-                    if image_type_correct(image_url):
-                        return image_url
-                    else:
-                        return False
-                else:
-                    return False
-            else:
-                return False
-        else:
-            return False
-    else:
-        return False
+    image_link = scraped_page.find('a', class_='image')
+    if image_link:
+        image = image_link.find('img')
+        if image and image.has_attr('data-file-width') and check_size(image):
+            image_url = get_page(image_link['href']).find('a', class_='internal')['href']
+            if image_type_correct(image_url):
+                return image_url
 
 
-def image_size_correct(image):
-    if int(image['data-file-width']) > 300:
+def check_size(image):
+    if int(image['data-file-width']) > 400:
         return True
-    else:
-        return False
 
 
-def image_type_correct(var):
-    if var.lower().endswith(('.png', '.jpg', '.jpeg')):
+def image_type_correct(image_url):
+    if image_url.lower().endswith(('.png', '.jpg', '.jpeg')):
         return True
-    else:
-        return False
 
 
 def save_image(image_url):
-    img_name = image_url.split('/')[-1]
-    f = open('img/' + img_name, 'wb')
+    image_name = image_url.split('/')[-1]
+    f = open('img/' + image_name, 'wb')
     f.write(requests.get('http:' + image_url).content)
     f.close()
-    return 'img/' + img_name
+    image = 'img/' + image_name
+    if os.path.getsize(image) > 3000000:
+        resize_image(image)
+    return image
+
+
+def resize_image(image):
+    f = open(image, 'r')
+    img = Image.open(f)
+    img = resizeimage.resize_width(img, 1000)
+    img.save(image, img.format)
+    f.close()
+
+
+def fits_in_tweet(text):
+    if len(text) > 116:
+        return False
+    else:
+        return True
 
 
 def write_tweet(title):
     if any(char.isdigit() for char in title):
-        text = "Does anyone think %s was good? %s!" % (title, random.choice(endings))
-        return text
+        text = "Does anyone really think %s was any good? %s!" % (title, random.choice(endings))
     else:
-        text = "The %s %s %s. %s!" % (random.choice(adjectives), title.title(), random.choice(insults), random.choice(endings))
+        text = "The %s %s %s %s!" % (random.choice(adjectives), title.title(), random.choice(insults), random.choice(endings))
+    if fits_in_tweet(text):
         return text
-
-
-def has_char_exception(title):
-    if any(char in title for char in exceptions):
-        return True
-    else:
-        return False
 
 
 def post_tweet(text, image):
     twitter = get_tokens()
-    image = open(image, 'rb')
-    response = twitter.upload_media(media=image)
+    photo = open(image, 'rb')
+    response = twitter.upload_media(media=photo)
     twitter.update_status(status=text, media_ids=[response['media_id']])
     print text
-    time.sleep(1)
-
-
-def fits_in_tweet(text):
-    chars = len(text)
-    if chars > 116:
-        return False
-    else:
-        return True
 
 
 def main():
-    page = get_page(RANDOM)
+    page = get_page(GET_RANDOM)
     title = get_title(page)
     image = get_image(page)
     if title and image:
-        if not has_char_exception(title):
-            text = write_tweet(title)
-            if fits_in_tweet(text):
-                image_path = save_image(image)
-                if os.path.getsize(image_path) <= 5000000:
-                    post_tweet(text, image_path)
-                else:
-                    main()
-            else:
-                main()
-        else:
-            main()
+        text = write_tweet(title)
+        image_path = save_image(image)
+        post_tweet(text, image_path)
     else:
         main()
 
